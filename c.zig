8e0183595ee821afbe53f09277dd47e082a380cb
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-06-26 23:07:55 +0300

    pool - cimgui needs some more defines after update

--- c.zig
+++ c.zig
@@ -1,5 +1,7 @@
 pub usingnamespace @cImport({
     @cDefine("CIMGUI_DEFINE_ENUMS_AND_STRUCTS", "");
+    @cDefine("CIMGUI_USE_SDL", "");
+    @cDefine("CIMGUI_USE_OPENGL3", "");
     @cInclude("cimgui.h");
     @cInclude("cimgui_impl.h");
 
