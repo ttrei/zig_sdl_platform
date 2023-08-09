3393b421dda1c30ffc6e25d77c610dae565dbf8b
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-08-09 20:56:26 +0300

    Rename

--- src/example.zig
+++ src/example.zig
@@ -26,7 +26,7 @@ const green = 0x00FF00FF;
 const blue = 0x0000FFFF;
 const yellow = 0xFFFF00FF;
 
-const PersistGlobal = struct {
+const Global = struct {
     var show_demo_window: bool = false;
     var scale: f32 = 1.0;
     var tone_a: f64 = TONE_A;
@@ -99,14 +99,14 @@ const Scene = struct {
 };
 
 pub fn main() !void {
-    PersistGlobal.scene = Scene.init();
-    defer PersistGlobal.scene.deinit();
+    Global.scene = Scene.init();
+    defer Global.scene.deinit();
 
-    PersistGlobal.scene.current_polygon = try PersistGlobal.scene.addPolygon();
-    PersistGlobal.scene.rectangle = try PersistGlobal.scene.addRectangle(
+    Global.scene.current_polygon = try Global.scene.addPolygon();
+    Global.scene.rectangle = try Global.scene.addRectangle(
         &.{ .p1 = .{ .x = 30, .y = 30 }, .p2 = .{ .x = 200, .y = 300 } },
     );
-    PersistGlobal.scene.circle = try PersistGlobal.scene.addCircle(
+    Global.scene.circle = try Global.scene.addCircle(
         &.{ .c = .{ .x = 70, .y = 150 }, .r = 60 },
     );
 
@@ -116,68 +116,68 @@ pub fn main() !void {
 fn update(step: f64) void {
     _ = step;
 
-    PersistGlobal.viewport.camera_transform.scale = PersistGlobal.scale;
+    Global.viewport.camera_transform.scale = Global.scale;
 }
 
 fn render() void {
-    PersistGlobal.buffer.clear(0xFFFFFFFF);
-    PersistGlobal.viewport.buffer.clear(0x000000FF);
+    Global.buffer.clear(0xFFFFFFFF);
+    Global.viewport.buffer.clear(0x000000FF);
 
-    PersistGlobal.scene.draw(&PersistGlobal.viewport) catch unreachable;
+    Global.scene.draw(&Global.viewport) catch unreachable;
 
-    _ = platform.c.igSliderFloat("scale", &PersistGlobal.scale, 0.5, 1.5, "%.02f", 0);
+    _ = platform.c.igSliderFloat("scale", &Global.scale, 0.5, 1.5, "%.02f", 0);
     // platform.imguiText("Area: {d:.2}", .{poly.area2()});
-    platform.imguiText("A: {d:.2} Hz", .{PersistGlobal.tone_a});
-    platform.imguiText("B: {d:.2} Hz", .{PersistGlobal.tone_b});
+    platform.imguiText("A: {d:.2} Hz", .{Global.tone_a});
+    platform.imguiText("B: {d:.2} Hz", .{Global.tone_b});
 
-    if (PersistGlobal.show_demo_window) platform.c.igShowDemoWindow(&PersistGlobal.show_demo_window);
+    if (Global.show_demo_window) platform.c.igShowDemoWindow(&Global.show_demo_window);
 }
 
 fn updateViewPort() void {
-    const viewport_width = PersistGlobal.buffer.width * 8 / 10;
-    const viewport_height = PersistGlobal.buffer.height * 8 / 10;
-    if (PersistGlobal.viewport_pos.x + viewport_width > PersistGlobal.buffer.width) {
-        PersistGlobal.viewport_pos.x = PersistGlobal.buffer.width - viewport_width;
+    const viewport_width = Global.buffer.width * 8 / 10;
+    const viewport_height = Global.buffer.height * 8 / 10;
+    if (Global.viewport_pos.x + viewport_width > Global.buffer.width) {
+        Global.viewport_pos.x = Global.buffer.width - viewport_width;
     }
-    if (PersistGlobal.viewport_pos.y + viewport_height > PersistGlobal.buffer.height) {
-        PersistGlobal.viewport_pos.y = PersistGlobal.buffer.height - viewport_height;
+    if (Global.viewport_pos.y + viewport_height > Global.buffer.height) {
+        Global.viewport_pos.y = Global.buffer.height - viewport_height;
     }
-    PersistGlobal.viewport = ViewPort{
+    Global.viewport = ViewPort{
         .camera_transform = CoordinateTransform{
             .translate_x = 50.0,
             .translate_y = -20.0,
             .scale = 1.5,
         },
-        .buffer = PersistGlobal.buffer.subBuffer(
+        .buffer = Global.buffer.subBuffer(
             viewport_width,
             viewport_height,
-            PersistGlobal.viewport_pos,
+            Global.viewport_pos,
         ) catch unreachable,
     };
 }
 
 fn resize(pixels: []u32, width: ScreenCoordinate, height: ScreenCoordinate) void {
-    PersistGlobal.buffer = PixelBuffer.init(pixels, width, height) catch unreachable;
+    Global.buffer = PixelBuffer.init(pixels, width, height) catch unreachable;
     updateViewPort();
 }
 
 fn processInput(input: *const InputState) void {
     if (input.key_space_down) {
-        PersistGlobal.viewport_pos.x += 5;
-        PersistGlobal.viewport_pos.y += 5;
+        Global.viewport_pos.x += 5;
+        Global.viewport_pos.y += 5;
         updateViewPort();
     }
     if (input.mouse_right_down) {
-        PersistGlobal.scene.current_polygon = PersistGlobal.scene.addPolygon() catch unreachable;
+        Global.scene.current_polygon = Global.scene.addPolygon() catch unreachable;
     }
     const pointer = PointInt{
         .x = input.mouse_x,
         .y = input.mouse_y,
     };
-    const pointer_scene = PersistGlobal.viewport.camera_transform.reverseInt(
-        &pointer.sub(&PointInt.fromPixel(&PersistGlobal.viewport_pos)),
+    const pointer_scene = Global.viewport.camera_transform.reverseInt(
+        &pointer.sub(&PointInt.fromPixel(&Global.viewport_pos)),
     );
-    var poly = PersistGlobal.scene.current_polygon;
+    var poly = Global.scene.current_polygon;
     if (input.mouse_left_down) {
         if (poly.n == 0) {
             poly.add_vertex(pointer_scene) catch unreachable;
@@ -185,7 +185,7 @@ fn processInput(input: *const InputState) void {
         poly.add_vertex(pointer_scene) catch unreachable;
     }
     if (input.mouse_middle_down) {
-        PersistGlobal.scene.circle.c = pointer_scene;
+        Global.scene.circle.c = pointer_scene;
     }
     if (poly.n > 0) {
         poly.first.prev.p.x = pointer_scene.x;
@@ -194,35 +194,35 @@ fn processInput(input: *const InputState) void {
 
     const factor_a = (@as(f64, @floatFromInt(input.controller_left_y)) + 32768) / 32768;
     const factor_b = (@as(f64, @floatFromInt(input.controller_right_y)) + 32768) / 32768;
-    PersistGlobal.tone_a = TONE_A * factor_a;
-    PersistGlobal.tone_b = TONE_B * factor_b;
+    Global.tone_a = TONE_A * factor_a;
+    Global.tone_b = TONE_B * factor_b;
     // std.debug.print("{d}\t{d}\n", .{ input.controller_left_x, input.controller_left_y });
 }
 
 fn writeAudio(buffer: *ApplicationAudioBuffer) void {
-    const Persist = struct {
+    const Local = struct {
         var phase_a: f64 = 0.0;
         var phase_b: f64 = 0.0;
     };
 
-    const period_a = @as(f32, @floatFromInt(AudioSettings.sample_rate)) / PersistGlobal.tone_a;
-    const period_b = @as(f32, @floatFromInt(AudioSettings.sample_rate)) / PersistGlobal.tone_b;
+    const period_a = @as(f32, @floatFromInt(AudioSettings.sample_rate)) / Global.tone_a;
+    const period_b = @as(f32, @floatFromInt(AudioSettings.sample_rate)) / Global.tone_b;
     const two_pi = 2 * std.math.pi;
 
     const frame_count = buffer.sample_count / AudioSettings.channel_count;
 
     var i: u32 = 0;
     while (i < frame_count) {
-        const amplitude_a = std.math.sin(Persist.phase_a) * PersistGlobal.tone_vol;
-        const amplitude_b = std.math.sin(Persist.phase_b) * PersistGlobal.tone_vol;
+        const amplitude_a = std.math.sin(Local.phase_a) * Global.tone_vol;
+        const amplitude_b = std.math.sin(Local.phase_b) * Global.tone_vol;
         const sample_value = @as(i16, @intFromFloat(amplitude_a + amplitude_b));
         var j: u8 = 0;
         while (j < AudioSettings.channel_count) {
             buffer.samples[AudioSettings.channel_count * i + j] = sample_value;
             j += 1;
         }
-        Persist.phase_a = @mod(Persist.phase_a + two_pi / period_a, two_pi);
-        Persist.phase_b = @mod(Persist.phase_b + two_pi / period_b, two_pi);
+        Local.phase_a = @mod(Local.phase_a + two_pi / period_a, two_pi);
+        Local.phase_b = @mod(Local.phase_b + two_pi / period_b, two_pi);
         i += 1;
     }
 }
