6d00ca4d471629c069351c6e5a0474c386bef206
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-11-15 22:13:55 +0200

    Specify include paths relative to the root
    Otherwise it breaks when compiled as a dependency within another project.

--- build.zig
+++ build.zig
@@ -66,18 +66,21 @@ pub fn link(sdk: *Platform, exe: *std.Build.LibExeObjStep) void {
     });
     // https://github.com/cimgui/cimgui/blob/261250f88f374e751b2de1501ba5c0c11e420b5a/backend_test/CMakeLists.txt#L39
     cimgui_sdl2_opengl3_obj.defineCMacro("IMGUI_IMPL_API", "extern \"C\"");
-    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = "vendor/cimgui" });
-    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = "vendor/cimgui/generator/output" });
-    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = "vendor/cimgui/imgui" });
-    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = "vendor/cimgui/imgui/backends" });
+    const root_dir = comptime blk: {
+        break :blk std.fs.path.dirname(@src().file) orelse ".";
+    };
+    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui" });
+    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui/generator/output" });
+    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui/imgui" });
+    cimgui_sdl2_opengl3_obj.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui/imgui/backends" });
     cimgui_sdl2_opengl3_obj.addCSourceFiles(.{ .files = &.{
-        "vendor/cimgui/imgui/imgui.cpp",
-        "vendor/cimgui/imgui/imgui_demo.cpp",
-        "vendor/cimgui/imgui/imgui_draw.cpp",
-        "vendor/cimgui/imgui/imgui_tables.cpp",
-        "vendor/cimgui/imgui/imgui_widgets.cpp",
-        "vendor/cimgui/imgui/backends/imgui_impl_sdl2.cpp",
-        "vendor/cimgui/imgui/backends/imgui_impl_opengl3.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/imgui.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/imgui_demo.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/imgui_draw.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/imgui_tables.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/imgui_widgets.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/backends/imgui_impl_sdl2.cpp",
+        root_dir ++ "/vendor/cimgui/imgui/backends/imgui_impl_opengl3.cpp",
     } });
     cimgui_sdl2_opengl3_obj.linkLibC();
     cimgui_sdl2_opengl3_obj.linkLibCpp();
@@ -85,7 +88,7 @@ pub fn link(sdk: *Platform, exe: *std.Build.LibExeObjStep) void {
     sdk.sdl_sdk.link(cimgui_sdl2_opengl3_obj, .static);
 
     exe.defineCMacro("IMGUI_IMPL_API", "extern \"C\"");
-    exe.addIncludePath(.{ .path = "vendor/cimgui" });
-    exe.addIncludePath(.{ .path = "vendor/cimgui/generator/output" });
+    exe.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui" });
+    exe.addIncludePath(.{ .path = root_dir ++ "/vendor/cimgui/generator/output" });
     exe.addObject(cimgui_sdl2_opengl3_obj);
 }
